6c9e7bf0055a402a59048f996a882debf0c20118
Lawrence L <Lawrence.E.Lee@gmail.com>
2021-11-26 22:26:17 -0800

    Created Pages contorller, view, and model from scratch. Also modified routes.rb to route pages to pages controller.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_18_055252) do
+ActiveRecord::Schema.define(version: 2021_11_27_060639) do
+
+  create_table "pages", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.string "slug"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
