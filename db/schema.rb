417efe5e1d7c5c360e154ea568208f6d2bd78f7b
Lawrence L <Lawrence.E.Lee@gmail.com>
2021-11-26 19:04:30 -0800

    Fixed typo error in views/posts. This completes the first set of videos for basic rails intro.

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_18_043015) do
+ActiveRecord::Schema.define(version: 2021_11_18_055252) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.text "body"
   end
 
 end
