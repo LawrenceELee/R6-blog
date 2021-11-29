4f5e37ffc6e1495cc9264e3fd121827f001aa2eb
Lawrence L <Lawrence.E.Lee@gmail.com>
2021-11-28 20:30:09 -0800

    Refactored new and edit into a _form partial.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -13,8 +13,21 @@ class PagesController < ApplicationController
   end
 
   def create
-    @page = Page.new(params)
-    render plain: params.to_json
+    # manually tell rails which params should be allowed to prevent malicious stuff
+    page_params = params.require(:page).permit(:title, :body, :slug)
+
+    # creates a new Page object with the accepted params
+    @page = Page.new(page_params)
+
+    # save the newly create Page object into model 
+    @page.save
+
+    # redirected to the show page for the new Page objectafter you submit form data
+    redirect_to @page
+  end
+
+  def edit
+    @page = Page.find(params[:id])
   end
 
 
